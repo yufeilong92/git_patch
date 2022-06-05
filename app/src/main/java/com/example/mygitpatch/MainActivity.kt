a6248b3053f93a97306a2103ba13000a03c5b414
931697478@qqcom <931697478@qqcom>
2022-06-05 12:29:20 +0800

    第二次提交

--- app/src/main/java/com/example/mygitpatch/MainActivity.kt
+++ app/src/main/java/com/example/mygitpatch/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.mygitpatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mygitpatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
